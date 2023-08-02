b7fdd8882fffddba97e5df1612afbb590379da87
jaerius <rylynn1029@naver.com>
2023-08-02 19:53:11 +0900

    test

--- server/scripts/generate.js
+++ server/scripts/generate.js
@@ -5,9 +5,17 @@
  */
 const secp = require("ethereum-cryptography/secp256k1");
 const { toHex } = require("ethereum-cryptography/utils");
+const { keccak256 } = require("ethereum-cryptography/keccak");
+
 
 const privateKey = secp.utils.randomPrivateKey();
 const publicKey = secp.getPublicKey(privateKey);
 
 console.log("private key : ", toHex(privateKey));
 console.log("public key  : ", toHex(publicKey));
+
+function getAddress(publicKey){
+	console.log("20 address: ",toHex(keccak256(publicKey.slice(1)).slice(-20)).toUpperCase()) ; 
+}
+
+getAddress(publicKey);
